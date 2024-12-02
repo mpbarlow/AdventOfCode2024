52743b1fea1ee5237eddfd5a43e20333181acb6f
mpbarlow <matt@barlow.coffee>
2024-12-02 22:40:12 +0000

    day 1

---
+++ Day1/main.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+let file = FileManager.default.currentDirectoryPath + "/Day1/input.txt"
+
+do {
+    let contents = try String(contentsOfFile: file, encoding: .utf8)
+    
+    var left = [Int]()
+    var right = [Int]()
+    
+    for line in contents.split(separator: "\n") {
+        let split = line.split(separator: "   ")
+        
+        left.append(Int(split[0])!)
+        right.append(Int(split[1])!)
+    }
+    
+    let totalDistance = zip(left.sorted(), right.sorted()).reduce(0) { carry, tuple in carry + abs(tuple.0 - tuple.1) }
+    
+    print(totalDistance)
+    
+    let freqMap = right.reduce([Int: Int]()) { map, r in
+        var map = map
+        map.updateValue(map[r, default: 0] + 1, forKey: r)
+        
+        return map
+    }
+    
+    let totalSimilarity = left.reduce(0) { c, l in
+        c + (l * freqMap[l, default: 0])
+    }
+    
+    print(totalSimilarity)
+} catch {
+    exit(1)
+}
